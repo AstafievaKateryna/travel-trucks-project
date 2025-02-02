5dd6cb6f1075539e53e52abf44b195bccc25f190
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2025-02-03 00:19:39 +0200

    favourite

--- src/components/common/camper/CamperPrice/CamperPrice.jsx
+++ src/components/common/camper/CamperPrice/CamperPrice.jsx
@@ -1,8 +1,25 @@
 import s from "./CamperPrice.module.css";
+import SvgIcon from "../../SvgIcon/SvgIcon";
+import React, { useState } from "react";
+
+const handleFavoriteClick = (itemId) => {
+  const newFavorites = favorites.includes(itemId)
+    ? favorites.filter((id) => id !== itemId)
+    : [...favorites, itemId];
+  setFavorites(newFavorites);
+  localStorage.setItem("favorites", JSON.stringify(newFavorites));
+};
 
 const CamperPrice = ({ item }) => {
-  return;
-  <p className={s.price}>€{item.price}.00</p>;
+  const [favorites, setFavorites] = useState(() => {
+    const saved = localStorage.getItem("favorites");
+    return saved ? JSON.parse(saved) : [];
+  });
+  return <p className={s.price}>€{item.price}.00</p>;
+  <svg
+    onClick={() => handleFavoriteClick(item.id)}
+    className={favorites.includes(item.id) ? s.liked : s.like}
+  ></svg>;
 };
 
 export default CamperPrice;
