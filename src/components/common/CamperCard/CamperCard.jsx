a66af0a86512c711af669ad7592f3216f2553e42
AstafievaKateryna <134802379+AstafievaKateryna@users.noreply.github.com>
2025-02-02 03:28:07 +0200

    CamperCard

--- src/components/common/CamperCard/CamperCard.jsx
+++ src/components/common/CamperCard/CamperCard.jsx
@@ -16,4 +16,37 @@ const CamperCard = ({ item }) => {
   const handleFavoriteChange = () => {
     dispatch(toggleFavorite(item.id));
   };
+  return (
+    <li className={s.card}>
+      <div className={s.imageContainer}>
+        <img src={item.gallery[0].thumb} alt="Truck Photo" className={s.img} />
+      </div>
+      <div className={s.cardContainer}>
+        <div className={s.cardInfo}>
+          <div className={s.head}>
+            <CamperHead item={item} />
+            <CamperPrice item={item} />
+          </div>
+          <p className={s.description}>{item.description}</p>
+          <CamperOptions item={item} />
+          <Button id={item.id}>Show more</Button>
+        </div>
+        <label className={s.favorite}>
+          <input
+            type="checkbox"
+            checked={isFavorite}
+            onChange={handleFavoriteChange}
+          />
+          <SvgIcon
+            id="icon-heart"
+            className={s.svgHeart}
+            width="25"
+            height="24"
+          />
+        </label>
+      </div>
+    </li>
+  );
 };
+
+export default CamperCard;
